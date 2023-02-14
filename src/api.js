60f40ce045d9e009222f644f7e819b648e62857f
Aryan Chourey <aryanchourey4@gmail.com>
2023-02-15 00:21:25 +0530

    api.js edit

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
+		'X-RapidAPI-Key': 'e25a58841cmshc719d7fa4d4f461p1a838fjsnd18f74d056a4',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -8,3 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = 'a5809a9e2bd365cc57a32fd532f36b4e';
